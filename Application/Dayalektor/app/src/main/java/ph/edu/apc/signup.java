d06520884ee144941d8ec85dc260dbec79da4523
Gail Haboc <fghaboc@gmail.com>
2017-08-06 15:31:22 +0800

    sample source

---
+++ Application/Dayalektor/app/src/main/java/ph/edu/apc/signup.java
@@ -0,0 +1,15 @@
+package ph.edu.apc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ph.edu.apc.myapplication.R;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
